3f43a1c1da2bee102149e78f1b658acbbd05cf97
H0LLYxW00D <ara1212@gmail.com>
2017-10-19 17:44:09 -0500

    Fixed Foward Failure issues
    Added: ExitOnForwardFailure=yes

--- Tunnel-Hunter.sh
+++ Tunnel-Hunter.sh
@@ -20,7 +20,7 @@ echo ""
 host="74.208.182.71"
 for i in $( echo -e "21\n22\n23\n20\n25\n53\n80\n110\n143\n443\n465\n587\n993\n995\n3128\n3389\n5060\n5500\n8443\n1494\n8080\n" ); 
   do
-    /usr/bin/ssh -o ConnectTimeout=10 -N -R 5$i:localhost:22 root@$host -p$i
+    /usr/bin/ssh -o ConnectTimeout=10 ExitOnForwardFailure=yes -N -R 5$i:localhost:22 root@$host -p$i
   done
   if [[ $? -eq 0 ]]; then
     echo Tunnel success on port $i
